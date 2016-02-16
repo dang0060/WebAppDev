78e05474b553858259480f925a3f4fed2508e170
Yawei <wang0399@algonquinlive.com>
2016-02-15 19:08:41 -0500

    Base project for 0.2.3

--- LFGIRL0.2.3/src/java/TestDB.java
+++ LFGIRL0.2.3/src/java/TestDB.java
@@ -1,8 +1,12 @@
 
 import dao.UsersDAO;
+import dao.GroupsDAO;
+import java.util.Scanner;
 import hibernate.dataobjects.Users;
 import java.util.List;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import hibernate.dataobjects.Groups;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -17,9 +21,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestDB {
     
     public static void main(String[] args) {
-        
+    
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml")) {
             UsersDAO usersDAO = context.getBean(UsersDAO.class);
+            GroupsDAO groupDao = context.getBean(GroupsDAO.class);
+            Scanner inputScanner = new Scanner(System.in);
             
             //User x = new User();
             //x.setUserId(150);
@@ -29,12 +35,30 @@ public class TestDB {
             
             //userDAO.save(x);
             
-            List<Users> list = usersDAO.listUsers();
+            /*List<Users> list = usersDAO.listUsers();
             
             for(Users u : list) {
                 System.out.println("User Name: " + u.getUsername());
-            }
+            }*/
+        // while(true){
+            System.out.println("Enter user name:");
+            Users foundUser = new Users();
+            foundUser = usersDAO.findUserByName(inputScanner.nextLine());
+            System.out.println(foundUser.getUsername());
+            System.out.println(foundUser.getUserId());
+            //System.out.println(foundGroup.getDescription());
+            
+          /* List<Groups> groupList = groupDao.listGroups();
+            for(Groups g : groupList) {
+                System.out.println("Group Name: " + g.getGroupname());
+            }*/
+            System.out.println("Enter group name:");
+            Groups foundGroup = new Groups();
+            foundGroup = groupDao.findGroupByName(inputScanner.nextLine());
+            System.out.println(foundGroup.getGroupname());
+            System.out.println(foundGroup.getGroupId());
+          //}
         }
     }
-    
+   
 }
--- LFGIRL0.2.3/src/java/hibernate/dataobjects/Groups.java
+++ LFGIRL0.2.3/src/java/hibernate/dataobjects/Groups.java
@@ -54,7 +54,7 @@ public class Groups  implements java.io.Serializable {
     }
 
     
-    @Column(name="groupname", nullable=false, length=100)
+    @Column(name="name", nullable=false, length=100) //changed from groupname to name
     public String getGroupname() {
         return this.groupname;
     }
